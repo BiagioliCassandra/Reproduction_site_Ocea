de09a9c3405a337c4a44a49d26ad395badd63822
BiagioliCassandra <43178222+BiagioliCassandra@users.noreply.github.com>
2018-10-05 13:41:22 +0200

    Add files via upload
    add html and text footer

--- index.php
+++ index.php
@@ -21,7 +21,20 @@
       Start header
 ~~~~~~~~~~~~~~~~~~~~~~-->
   <header>
-
+    <nav>
+      <ul>
+        <li><i></i></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+    <div>
+      <div>
+        <p></p>
+      </div>
+    </div>
   </header>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End header
@@ -30,7 +43,21 @@
       Start main
 ~~~~~~~~~~~~~~~~~~~~~~-->
   <main>
-
+  <!--the two div of the top bar that scrolls-->
+    <div>
+      <aside>
+        <i></i>
+        <i></i>
+        <i></i>
+      </aside>
+    </div>
+    <div>
+      <aside>
+        <form>
+              
+        </form>
+      </aside>
+    </div>
   </main>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End main
@@ -39,7 +66,53 @@
       Start footer
 ~~~~~~~~~~~~~~~~~~~~~~-->
   <footer>
-
+    <div>
+      <section>
+        <h3>Ocea</h3>
+        <ul>
+          <li>51 rue de la Cimaise</li>
+          <li>59650 Villeneuve d'Ascq</li>
+          <li>03.20.47.03.03</li>
+          <li>accueil at oceaclub.com</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Ocea Sport</h3>
+        <ul>
+          <li><a>Le planning</a></li>
+          <li><a>Les cours</a></li>
+          <li><a>Les tarifs</a></li>
+          <li><a>L'offre découverte</a></li>
+          <li><a>Réserver un cours de badminton</a></li>
+          <li><a>Réserver un atelier</a></li>
+          <li><a>Résilier</a></li>
+        </ul>
+      </section>
+      <section>
+        <h3>Ocea Esthétique</h3>
+        <ul>
+          <li><a>Solutions Beauté Visage</a></li>
+          <li><a>Solutions Beauté Corps</a></li>
+          <li><a>Expériences Payot</a></li>
+          <li><a>Modelages "Bien-Etre"</a></li>
+          <li><a>Formules Complètes "Forfaits"</a></li>
+          <li><a>Mains, Pieds et Ongles</a></li>
+          <li><a>Épilations</a></li>
+          <li><a>Maquillage, Beauté du regard</a></li>
+        </ul>
+      </section>
+      <section>
+        <h3>Ocea Sun</h3>
+        <ul>
+          <li><a>Les machines</a></li>
+          <li><a>Les tarifs</a></li>
+          <li><a>Les produits</a></li>
+          <li><a>Réserver une séance</a></li>
+        </ul>
+      </section>
+    </div>
+    <hr>
+    <p>© 2018 Oxygene Studio - C.G.V. - Mentions Légales</p>
   </footer>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End footer
