e39b2fc4bd8c41957a531e93d35491f8f1c4c487
BiagioliCassandra <43178222+BiagioliCassandra@users.noreply.github.com>
2018-10-07 13:42:10 +0200

    Add css for the main

--- index.php
+++ index.php
@@ -20,7 +20,9 @@
 <!--~~~~~~~~~~~~~~~~~~~~~
       Start header
 ~~~~~~~~~~~~~~~~~~~~~~-->
-  
+<?php 
+  include("header.php"); 
+?>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End header
 ~~~~~~~~~~~~~~~~~~~~~~-->
@@ -31,9 +33,9 @@
   <!--the two div of the top bar that scrolls-->
     <div class="flex">
       <aside>
-        <i></i>
-        <i></i>
-        <i></i>
+        <i><a href="http://www.facebook.com/oceaclub"></a></i>
+        <i><a href="http://www.oceaclub.com/contact.php"></a></i>
+        <i><a href="http://www.oceaclub.com/acces.php"></a></i>
       </aside>
     </div>
     <div class="flex">
@@ -43,38 +45,303 @@
         </form>
       </aside>
     </div>
-  <!--The section ocea sport containing 4 div-->
-    <section>
-      <h1 id="oceasport" class="center">Ocea Sport</h1>
-      <div class="flex">
-        <i></i>
-        <p>Un club ouvert 7 jours sur 7</p>
+  <!--Section: oceasport. The section ocea sport containing 4 div with icones and p-->
+    <section class="backgroundcolor_pink">
+      <h1 id="oceasport" class="center color_white">Ocea Sport</h1>
+      <div class="flex space_around">
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Un club ouvert 7 jours sur 7</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Plus de 100 cours collectifs<br> chaque semaine</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">6 salles dont un studio RPM avec<br> plus de 70 vélos</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">7000 m² d'infrastructures</p>
+        </div>
       </div>
-      <div class="flex">
-        <i></i>
-        <p>Plus de 100 cours collectifs<br> chaque semaine</p>
-      </div>
-      <div class="flex">
-        <i></i>
-        <p>6 salles dont un studio RPM avec<br> plus de 70 vélos</p>
-      </div>
-      <div class="flex">
-        <i></i>
-        <p>7000 m² d'infrastructures</p>
-      </div>
-      <a class="center a" href="http://www.oceaclub.com/decouverte.php">TESTEZ-NOUS !</a>
+      <p class="center"><a class="a color_black" href="http://www.oceaclub.com/decouverte.php">TESTEZ-NOUS !</a></p>
     </section>
-  <!--the section "notre contrat" containing h2, p and video-->
+  <!--Sous-section: oceasport. The section "notre contrat" containing h2, p and video-->
     <section></section>
-      <h2>Notre contrat</h2>
+      <h3>Notre contrat</h3>
       <p>Pas de sentiment d'abandon à Ocea ! Nos nombreux éducateurs sportifs, tous diplômés d'état, sont à votre disposition pour atteindre vos objectifs, vous conseiller et<br> vous guider tout au long de votre entrainement.</p>
-      <div>
+      <div class="center">
         <iframe src="https://player.vimeo.com/video/272767022" width="640" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       </div>
-  <!---->
-    <section>
-
+  <!--Sous-section: oceasport. The 4 articles "planing, cours, tarifs et offre découverte"-->
+    <section class="backgroundcolor_pink flex space_around">
+      <article>
+        <header>
+          <h3 class="center color_white">Le planning</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/planning.jpg" alt="Une image de planning" width="250">
+        <p class="color_white">Plus de 100 cours collectifs à votre<br> disposition</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/planning.php">LE PLANNING</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Les cours</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/cours.jpg" alt="L'image d'une fille qui fait du sport et qui sourie" width="250">
+        <p class="color_white">20 types de cours différents et ce qui<br> se fait de meilleur au monde</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/cours.php">LES COURS</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Les tarifs</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/tarifs.jpg" alt="Une image avec écrit dès 29 euros par mois" width="250">
+        <p class="color_white">Nous avons forcément le meilleur<br> tarif adapté à vos ambitions</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/tarifs.php">LES TARIFS</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">L'offre découverte</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/decouverte.jpg" alt="Une image de sportifs qui boivent de l'eau" width="250">
+        <p class="color_white">A Ocea, votre première séance vous<br> est offerte</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/decouverte.php">VOIR L'OFFRE</a>
+      </article>
     </section>
+  <!--Sous-section: oceasport. A div containing 2 sections "horaires et s'y rendre"-->
+    <div class="flex">
+      <section>
+        <h3>Les horaires</h3>
+        <ul>
+          <li>Du lundi au jeudi de 9h à 21h30</li>
+          <li>Le vendredi de 9h à 21h</li>
+          <li>Le samedi et le dimanche de 9h à 20h</li>
+        </ul>
+        <p>Tel. : <span class="color_pink">03.20.47.03.03</span></p>
+      </section>
+      <section>
+        <h3>S'y rendre</h3>
+        <ul class="puce">
+          <li>51 rue de la Cimaise</li>
+          <li>Villeneuve d'Ascq</li>
+          <li><a class="a color_pink" href="http://www.oceaclub.com/acces.php">» Accès</a></li>
+        </ul> 
+      </section>
+    </div>
+  <!--A img who separate the section sport and the section esthetic-->
+    <div>
+      <img scr="http://www.oceaclub.com/slides/esth.jpg" alt="L'image d'une femme qui se fait masser" width="500">
+    </div>
+  <!--Section: ocea esthetic. A section containing 4 div with icones and p-->
+    <section class="backgroundcolor_purple">
+      <h2 id="oceaesth" class="center color_white">Ocea Esthétique</h2>
+      <div class="flex space_around">
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Un panel large de soins de qualité</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Des esthéticiennes diplômées</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Des prestations non chronométrées</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Une offre pour femme et pour homme</p>
+        </div>
+        </div>
+    </section>
+  <!--Sous-section: ocea esthetic. A section "notre offre"-->
+    <section>
+      <h3>Notre offre</h3>
+      <p>Relaxation, beauté, bien-être, Ocea Esthétique se met à votre service au sein de notre pôle Bien-être pour vous donner un teint resplendissant.
+      Relaxez-vous, éclairez-vous ! Toute prise de rendez-vous se fait directement par téléphone au 03.20.47.46.45.</p>
+    </section>
+  <!--Sous-section: ocea esthetic. A section with 8 articles-->
+    <section class="backgroundcolor_purple flex space_around">
+    <article>
+        <header>
+          <h3 class="center color_white">Solutions Beauté Visage</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth1.jpg" alt="Une image d'une femme qui touche son visage" width="250">
+        <p class="color_white">Alliance de techniques et d’actifs<br> d’exception</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=1">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Solutions Beauté Corps</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth2.jpg" alt="Une image d'une femme qui fait un soin du dos" width="250">
+        <p class="color_white">Un programme ciblé à personnaliser<br> selon vos besoins</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=2">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Expériences Payot</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth3.jpg" alt="Une image d'une femme qui se fait masser le visage" width="250">
+        <p class="color_white">Alliance parfaite entre performance<br> et effervescence des sens</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=3">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Modelages "Bien-Etre"</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth4.jpg" alt="Une image d'une femme qui se fait masser le dos" width="250">
+        <p class="color_white">Techniques de modelage et<br> d’expériences poly-sensorielles</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=4">DECOUVRIR</a>
+      </article>
+    </section>
+    <section class="backgroundcolor_purple flex space_around">
+      <article>
+        <header>
+          <h3 class="center color_white">Formules Complètes "Forfaits"</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth5.jpg" alt="Une image d'une femme souriante et détendue" width="250">
+        <p class="color_white">Pour une expérience complète de<br> bien-être</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=5">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Mains, Pieds et Ongles</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth6.jpg" alt="Une image de pieds manicurés" width="250">
+        <p class="color_white">L'entretien parfait de votre manucure,<br> pieds et mains</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=6">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Épilations</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth7.jpg" alt="Une image de deux jambes parfaitement épilées" width="250">
+        <p class="color_white">Le soin de base avec qualité et<br> précision</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=7">DECOUVRIR</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Maquillage, Beauté du regard</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/esth8.jpg" alt="Une image d'un oeil maquillé" width="250">
+        <p class="color_white">Sublimez votre visage aux yeux de<br> tous</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/esthetique.php?c=8">DECOUVRIR</a>
+      </article>
+    </section>
+  <!--Sous-section: ocea esthetic. A div containing 2 sections "horaires et s'y rendre"-->
+    <div class="flex">
+      <section>
+        <h3>Les horaires</h3>
+        <ul>
+          <li>Du lundi au mercredi de 10h à 19h30</li>
+          <li>Le jeudi et le vendredi de 10h à 20h</li>
+          <li>Le samedi de 9h à 18h</li>
+        </ul>
+        <p>Tel. : <span class="color_purple">03.20.47.46.45</span></p>
+      </section>
+      <section>
+        <h3>S'y rendre</h3>
+        <ul class="puce">
+          <li>51 rue de la Cimaise</li>
+          <li>Villeneuve d'Ascq</li>
+          <li><a class="a color_pink" href="http://www.oceaclub.com/acces.php">» Accès</a></li>
+        </ul> 
+      </section>
+    </div>
+  <!--A img who separate the section esthetic and the section sun-->
+    <div>
+      <img scr="http://www.oceaclub.com/slides/sun.jpg" alt="L'image d'une femme qui fait des UVs" width="500">
+    </div>  
+  <!--Section: ocea sun. A section containing 4 div with icones and p-->
+  <section class="backgroundcolor_orange">
+      <h2 id="oceasun" class="center color_white">Ocea Sun</h2>
+      <div class="flex space_around">
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Machines UV Ergoline dernière génération</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Avec ou sans rendez-vous</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Des produits et soins variés</p>
+        </div>
+        <div>
+          <i class="color_white"></i>
+          <p class="color_white">Des esthéticiennes diplômées pour des conseils avisés</p>
+        </div>
+      </div>
+    </section>
+  <!--Sous-section: ocea sun. A section "notre offre"-->
+    <section>
+      <h3>Notre offre</h3>
+      <p>Le centre de bronzage Ocea Sun vous propose 3 machines UV Ergoline dernière génération de haute performance pour un teint hâlé de rêve.
+      Nous mettons à votre disposition 3 appareils (575, 700 et 950) avec ou sans rendez-vous au sein de notre pôle Bien-être.
+      Nos esthéticiennes diplômées et qualifiées sont à votre écoute pour des conseils personnalisés.</p>
+    </section>  
+  <!--Sous-section: ocea sun. The 4 articles "nos machines, les tarifs, brozag'expert et votre réservation"-->
+  <section class="backgroundcolor_orange flex space_around">
+      <article>
+        <header>
+          <h3 class="center color_white">Nos machines</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/machines.jpg" alt="Une image d'une machne UV'" width="250">
+        <p class="color_white">Profitez de nos machines dernière<br> génération en toute sécurité</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/machines.php">LES MACHINES</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Les tarifs</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/tarifs_sun.jpg" alt="Une image affichant des prix" width="250">
+        <p class="color_white">Votre séance U.V. selon votre profil au<br> meilleur prix</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/tarifs_sun.php">LES TARIFS</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Bronzag'expert</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/bronzag.jpg" alt="Une image d'une fille dans une piscine avec des lunettes de soleil" width="250">
+        <p class="color_white">Nouveau ! L'autobronzant au résultat<br> instantané et naturel</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/bronzage.php">LE DETAIL</a>
+      </article>
+      <article>
+        <header>
+          <h3 class="center color_white">Votre réservation</h3>
+        </header>
+        <img src="http://www.oceaclub.com/covers/sunresa.jpg" alt="Une image d'un planning" width="250">
+        <p class="color_white">Nous vous proposons l'opportunité<br> de réserver directement en ligne</p>
+        <a class="center a color_black" href="http://www.oceaclub.com/sun/" target="_blank">RESERVER</a>
+      </article>
+    </section>  
+    <aside class="backgroundcolor_orange">
+      <p class="color_white">Attention ! L'exposition aux rayonnements d'un appareil de bronzage peut provoquer des cancers de la peau et des yeux et est responsable d'un vieillissement cutané prématuré. L'existence d'une réglementation du bronzage artificiel ne permet pas d'éliminer les risques sanitaires encourus en cas d'exposition, en particulier le risque de cancer. L'utilisation de ces appareils est interdite aux personnes de moins de 18 ans. Porter les lunettes de protection fournies.</p>
+    </aside>
+  <!--Sous-section: ocea sun. A div containing 2 sections "horaires et s'y rendre"-->
+  <div class="flex">
+      <section>
+        <h3>Les horaires</h3>
+        <ul>
+          <li>Du lundi au mercredi de 10h à 19h30</li>
+          <li>Le jeudi et le vendredi de 10h à 20h</li>
+          <li>Le samedi de 9h à 18h</li>
+        </ul>
+        <p>Tel. : <span class="color_orange">03.20.47.46.45</span></p>
+      </section>
+      <section>
+        <h3>S'y rendre</h3>
+        <ul class="puce">
+          <li>51 rue de la Cimaise</li>
+          <li>Villeneuve d'Ascq</li>
+          <li><a class="a color_pink" href="http://www.oceaclub.com/acces.php">» Accès</a></li>
+        </ul> 
+      </section>
+    </div> 
   </main>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End main
@@ -82,7 +349,9 @@
 <!--~~~~~~~~~~~~~~~~~~~~~
       Start footer
 ~~~~~~~~~~~~~~~~~~~~~~-->
-<?php include("footer.php"); ?>
+<?php 
+  include("footer.php"); 
+?>
 <!--~~~~~~~~~~~~~~~~~~~~~
       End footer
 ~~~~~~~~~~~~~~~~~~~~~~-->
